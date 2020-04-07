ff3d1c7921b828aed01aa3e32faa4a4fa37a343f
Merouane Bellaha <m.bellaha@gmail.com>
2020-04-07 14:06:23 +0200

    Initial Commit

---
+++ RpgProjectCli/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  RpgProjectCli
+//
+//  Created by Merouane Bellaha on 07/04/2020.
+//  Copyright © 2020 Merouane Bellaha. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
